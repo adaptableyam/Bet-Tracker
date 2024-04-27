c9d6090dc9d272cfea8b66e29b96f5ebb38511aa
adaptableyam <stephensonrobert066@gmail.com>
2024-04-27 18:22:28 +0800

    Added and registered the Bet model, as well as created a superuser

--- tracker/models.py
+++ tracker/models.py
@@ -1,5 +1,41 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
-class Bet: 
-    pass
+"""
+The bet class needs:
+- type: Sports, roulette, etc
+- amount: float e.g £19.50
+- odds: e.g 3/1 (to convert odds to decimal its numerator/denominator + 1) so 3/1 would be 4 in decimal
+- win: true/false (false by default) 
+    if false, loss column is just amount. 
+    if true, profit is (decimal odds * amount - original bet amount)
+- profit: if no win, = 0. otherwise use formula above
+- loss: if win = 0. otherwise, = amount
+"""
+class Bet(models.Model): 
+    class Game(models.TextChoices):
+        ROULETTE = "RLT", "Roulette"
+        BLACKJACK = "BKJ", "Blackjack"
+        BACCARAT = "BAC", "Baccarat"
+        SLOTS = "SLT", "Slots"
+        CRAPS = "CPS", "Craps"
+        POKER = "PKR", "Poker"
+        SPORTS = "SPT", "Sports"
+        OTHER = "OTH", "Other"
+    # Offer a predetermined selection of most popular games, to make showing data easier.
+    game = models.CharField(max_length=100, choices=Game.choices, default=Game.SPORTS)
+    # Amount wagered up to 10,000,000.00
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    # keeping odds to decimal in order to speed up development, although less commonly used than ratio odds
+    decimal_odds = models.DecimalField(max_digits=8, decimal_places=2)
+    # Simple Win or Loss box, defaults to loss
+    win = models.BooleanField(default=False)
+    # Profit column, will be negative value of 'amount' if 'win' == false
+    # if 'win' == True: profit = amount * odds      ** Need to convert odds to 
+    profit = models.DecimalField(max_digits=12, decimal_places=2)
+    # Imported datetime module to get an overridable default value of current user date.
+    bet_date = models.DateField(default=timezone.now)
+
+
+
